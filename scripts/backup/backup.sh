91983fe513baa6b7fb07193524dc8b347a96b01c
Bobur Yusupov <bobur.yu06@mail.ru>
2024-10-20 18:08:01 +0200

    updates in code of backup

--- scripts/backup/backup.sh
+++ scripts/backup/backup.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-if [$# -ne 2]; then
-    echo "Usage: $0 <source_directory> <backup_directory>"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+if [ $# -ne 2 ]; then
+    echo -e "${RED}Error: Two arguments are required.${NC}"
+    echo -e "${BLUE}Usage: $0 <source_directory> <backup_directory>${NC}"
     exit 1
 
 fi
@@ -24,7 +30,9 @@ mkdir -p "$BACKUP_PATH"
 cp -r "$SOURCE_DIR/"* "$BACKUP_PATH/"
 
 if [ $? -eq 0 ]; then
-    echo "Backup of '$SOURCE_DIR' created at '$BACKUP_PATH'."
+    echo
+    echo -e "${GREEN}Success: Backup of '$SOURCE_DIR' created at '$BACKUP_PATH'.${NC}"
+    echo
 
 else
     echo "Error during backup."
